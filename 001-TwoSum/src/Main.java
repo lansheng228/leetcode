337da6b45ee0df3c4f07d802269d034559f39480
penghuaping170104 <penghuaping170104@credithc.com>
2018-02-11 19:10:47 +0800

    修改模块名和目录名，以方便linux使用

--- 001-TwoSum/src/Main.java
+++ 001-TwoSum/src/Main.java
@@ -4,6 +4,7 @@
  * Time: 09:17
  * Declaration: All Rights Reserved !!!
  */
+
 public class Main {
     public static void main(String[] args) {
         int[] result = new Solution().twoSum(new int[]{0, 4, 3, 0}, 0);
@@ -11,3 +12,5 @@ public class Main {
         System.out.println("[" + result[0] + " ," + result[1] + "]");
     }
 }
+
+
